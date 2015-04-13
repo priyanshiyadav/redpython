406f9419332a62e6a6ccc5e50ee937405174ef5d
redashu <asashuthegreat4@gmail.com>
2015-04-13 08:37:26 +0530

    this is really new python

---
+++ sock.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+print  "learn to lead !!"
+print   "                startup is the first step"
+print  "                                            this is really awesome"
+print  "                                   "
+print  "Be        Creatvie as you "
